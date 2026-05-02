10c44a9e7e7172951dbc0b6b590c5432bcfdad40
shar2025 <mail.sharathvittal@gmail.com>
2026-05-01 18:32:54 -0700

    seo: sitemap serialize, schema upgrade, breadcrumb JSON-LD, theme color, gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://sulacatering.com',
-  integrations: [sitemap()],
+	site: 'https://sulacatering.com',
+	integrations: [
+		sitemap({
+			serialize(item) {
+				if (item.url === 'https://sulacatering.com/') return item;
+				item.url = item.url.replace(/\/$/, '');
+				return item;
+			},
+		}),
+	],
 });
